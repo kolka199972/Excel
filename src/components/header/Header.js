8f45b92f6703f1880e16ae338f517e0b3cb00a22
kolka72 <kolia199972@gmail.com>
2022-02-17 16:15:58 +0300

    Finish create basic framework

---
+++ src/components/header/Header.js
@@ -0,0 +1,22 @@
+import { ExcelComponent } from '@core/ExcelComponent'
+
+export class Header extends ExcelComponent {
+  static className = 'excel__header'
+
+  toHTML() {
+    return `
+    <input type="text" class="input" value="Новая Таблица" />
+
+      <div>
+
+        <div class="button">
+          <span class="material-icons">delete</span>
+        </div>
+
+        <div class="button">
+          <span class="material-icons">logout</span>
+        </div>
+      </div>
+    `
+  }
+}
