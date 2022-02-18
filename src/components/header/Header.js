965eeab6b9decb5fd3757cd9235d47928c54314a
kolka72 <kolia199972@gmail.com>
2022-02-18 11:47:01 +0300

    Finish table resize

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,12 @@ import { ExcelComponent } from '@core/ExcelComponent'
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
+  constructor($root) {
+    super($root, {
+      name: 'Header',
+      listeners: []
+    })
+  }
 
   toHTML() {
     return `
