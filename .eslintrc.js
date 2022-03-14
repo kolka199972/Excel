1cb8a0af53eb4d33ab7706bf87e799ddb1c9c699
kolka72 <kolia199972@gmail.com>
2022-03-14 19:52:21 +0300

    create unit testing env

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,10 @@ module.exports = {
     }
   },
   env: {
-    browser: true,
-    node: true,
-    es6: true
+    'browser': true,
+    'node': true,
+    'es6': true,
+    'jest/globals': true
   },
   extends: ['eslint:recommended', 'google'],
   rules: {
@@ -20,5 +21,6 @@ module.exports = {
     'object-curly-spacing': 'off',
     'require-jsdoc': 'off',
     'operator-linebreak': 'off'
-  }
+  },
+  plugins: ['jest']
 }
